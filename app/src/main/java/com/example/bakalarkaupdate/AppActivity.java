82aafbfaf0e688d7ac0d4656cc3172125679f011
SarahBrlejova <brlejova.sarah@gmail.com>
2025-03-05 18:46:54 +0100

    navigation and fragments

--- app/src/main/java/com/example/bakalarkaupdate/AppActivity.java
+++ app/src/main/java/com/example/bakalarkaupdate/AppActivity.java
@@ -7,8 +7,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AppActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,36 @@ public class AppActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+
+        if (user == null) {
+            finish();
+        }
+        changeFragment(new CentersFragment());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_profil) {
+                changeFragment(new ProfilFragment());
+            } else if (itemId == R.id.nav_collections) {
+                changeFragment(new CollectionsFragment());
+            } else if (itemId == R.id.nav_training) {
+                changeFragment(new TrainingFragment());
+            } else if (itemId == R.id.nav_centers) {
+                changeFragment(new CentersFragment());
+            }
+            return true;
+        });
+
+    }
+
+    private void changeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
